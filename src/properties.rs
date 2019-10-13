8043055da0704159da648eefe75b77ded78e4e9c
stuart <stu@reilly-family.co.uk>
2019-10-13 18:54:55 +0100

    Add enum defs for selection props

--- src/properties.rs
+++ src/properties.rs
@@ -1,4 +1,27 @@
+use enum_map::{Enum, EnumMap, enum_map};
+use lazy_static::lazy_static;
+
 use std::fmt;
+
+lazy_static! {
+    static ref DEFAULT: EnumMap<SelectionProperty, Preference> = enum_map! {
+                SelectionProperty::Reliability => Preference::Require,
+                SelectionProperty::PreserveMsgBoundaries => Preference::Prefer,
+                SelectionProperty::PerMsgReliability => Preference::Ignore,
+                SelectionProperty::PreserveOrder => Preference::Require,
+                SelectionProperty::ZeroRttMsg => Preference::Ignore,
+                SelectionProperty::Multistreaming => Preference::Prefer,
+                SelectionProperty::PerMsgChecksumLenSend => Preference::Require,
+                SelectionProperty::PerMsgChecksumLenRecv => Preference::Require,
+                SelectionProperty::CongestionControl => Preference::Require,
+                SelectionProperty::Interface => Preference::Ignore,
+                SelectionProperty::Pvd => Preference::Ignore,
+                SelectionProperty::Multipath => Preference::Prefer,
+                //SelectionProperty::Direction(Direction::Bidirectional) => Preference::Require,
+                SelectionProperty::RetransmitNotify => Preference::Ignore,
+                SelectionProperty::SoftErrorNotify => Preference::Ignore,
+            };
+}
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 pub enum Preference {
@@ -9,7 +32,43 @@ pub enum Preference {
     Prohibit,
 }
 
-pub struct TransportProperties {}
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+pub enum Direction {
+    Bidirectional,
+    Sender,
+    Receiver
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, Enum)]
+pub enum SelectionProperty {
+    Reliability,
+    PreserveMsgBoundaries,
+    PerMsgReliability,
+    PreserveOrder,
+    ZeroRttMsg,
+    Multistreaming,
+    PerMsgChecksumLenSend,
+    PerMsgChecksumLenRecv,
+    CongestionControl,
+    Interface,
+    Pvd,
+    Multipath,
+    //Direction(Direction),
+    RetransmitNotify,
+    SoftErrorNotify,
+}
+
+pub struct TransportProperties {
+    map: EnumMap<SelectionProperty, Preference>
+}
+
+impl Default for TransportProperties {
+    fn default() -> Self {
+        TransportProperties {
+            map: DEFAULT.clone()
+        }
+    }
+}
 
 impl fmt::Display for Preference {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
@@ -20,5 +79,37 @@ impl fmt::Display for Preference {
             Preference::Avoid => f.write_str("Avoid"),
             Preference::Prohibit => f.write_str("Prohibit"),
         }
+    }
+}
+
+impl TransportProperties {
+    pub fn add(&mut self, prop: SelectionProperty, pref: Preference) -> &mut Self {
+        self.map[prop] = pref;
+        self
+    }
+
+    pub fn require(&mut self, prop: SelectionProperty) -> &mut Self {
+        self.add(prop, Preference::Require)
+    }
+
+    pub fn prefer(&mut self, prop: SelectionProperty) -> &mut Self {
+        self.add(prop, Preference::Prefer)
+    }
+
+    pub fn ignore(&mut self, prop: SelectionProperty) -> &mut Self {
+        self.add(prop, Preference::Ignore)
+    }
+
+    pub fn avoid(&mut self, prop: SelectionProperty) -> &mut Self {
+        self.add(prop, Preference::Avoid)
+    }
+
+    pub fn prohibit(&mut self, prop: SelectionProperty) -> &mut Self {
+        self.add(prop, Preference::Prohibit)
+    }
+
+    pub fn default(&mut self, prop: SelectionProperty) -> &mut Self {
+        let pref = DEFAULT[prop];
+        self.add(prop, pref)
     }
 }
