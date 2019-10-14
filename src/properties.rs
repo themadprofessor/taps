0c3f7dac491bff09695cf5f02a93b8db7f4f064f
stuart <stu@reilly-family.co.uk>
2019-10-14 09:43:58 +0100

    Add direction to transport props Add docs for default values

--- src/properties.rs
+++ src/properties.rs
@@ -17,7 +17,6 @@ lazy_static! {
                 SelectionProperty::Interface => Preference::Ignore,
                 SelectionProperty::Pvd => Preference::Ignore,
                 SelectionProperty::Multipath => Preference::Prefer,
-                //SelectionProperty::Direction(Direction::Bidirectional) => Preference::Require,
                 SelectionProperty::RetransmitNotify => Preference::Ignore,
                 SelectionProperty::SoftErrorNotify => Preference::Ignore,
             };
@@ -62,26 +61,46 @@ pub enum SelectionProperty {
     /// This property specifies whether the application needs to use a transport protocol that
     /// ensures that all data is received on the other side without corruption. This also entails
     /// being notified when a Connection is closed or aborted.
-    Reliability,
-
-    /// Preservation of message boundaries
     ///
-    /// This property specifies whether the application needs or prefers to  use a transport
-    /// protocol that preserves message boundaries.
-    PreserveMsgBoundaries,
+    /// # Default
+    ///
+    /// By default, this is `Require`. Changes to default values are **not** considered a breaking
+    /// change.
+    Reliability,
 
     /// Configure per-message reliability
     ///
     /// This property specifies whether an application considers it useful to indicate its
     /// reliability requirements on a per-Message basis. This property applies to Connections and
     /// Connection Groups.
+    ///
+    /// # Default
+    ///
+    /// By default, this is `Prefer`. Changes to default values are **not** considered a breaking
+    /// change.
     PerMsgReliability,
+
+    /// Preservation of message boundaries
+    ///
+    /// This property specifies whether the application needs or prefers to  use a transport
+    /// protocol that preserves message boundaries.
+    ///
+    /// # Default
+    ///
+    /// By default, this is `Ignore`. Changes to default values are **not** considered a breaking
+    /// change.
+    PreserveMsgBoundaries,
 
     /// Preservation of data ordering
     ///
     /// This property specifies whether the application wishes to use a transport protocol that can
     /// ensure that data is received by the application on the other end in the same order as it was
     /// sent.
+    ///
+    /// # Default
+    ///
+    /// By default, this is `Require`. Changes to default values are **not** considered a breaking
+    /// change.
     PreserveOrder,
 
     /// Use 0-RTT session establishment with an idempotent message
@@ -90,6 +109,11 @@ pub enum SelectionProperty {
     /// transport protocol before Connection establishment, which will then be reliably transferred
     /// to the other side before or during Connection establishment, potentially multiple times
     /// (i.e., multiple copies of the message data may be passed to the Remote Endpoint).
+    ///
+    /// # Default
+    ///
+    /// By default, this is `Ignore`. Changes to default values are **not** considered a breaking
+    /// change.
     ZeroRttMsg,
 
     /// Multistream connections in group
@@ -97,6 +121,11 @@ pub enum SelectionProperty {
     /// This property specifies that the application would prefer multiple Connections within a
     /// Connection Group to be provided by streams of a single underlying transport connection where
     /// possible.
+    ///
+    /// # Default
+    ///
+    /// By default, this is `Prefer`. Changes to default values are **not** considered a breaking
+    /// change.
     Multistreaming,
 
     /// Full checksum coverage on sending
@@ -104,12 +133,22 @@ pub enum SelectionProperty {
     /// This property specifies whether the application desires protection against corruption for
     /// all data transmitted on this Connection. Disabling this property may enable to control
     /// checksum coverage later.
+    ///
+    /// # Default
+    ///
+    /// By default, this is `Require`. Changes to default values are **not** considered a breaking
+    /// change.
     PerMsgChecksumLenSend,
 
     /// Full checksum coverage on receiving
     ///
     /// This property specifies whether the application desires protection against corruption for
     /// all data received on this Connection.
+    ///
+    /// # Default
+    ///
+    /// By default, this is `Require`. Changes to default values are **not** considered a breaking
+    /// change.
     PerMsgChecksumLenRecv,
 
     /// Congestion control
@@ -120,6 +159,11 @@ pub enum SelectionProperty {
     /// [RFC2914](https://tools.ietf.org/html/rfc2914).  Also note that reliability is usually
     /// combined with congestion control in protocol implementations, rendering "reliable but not
     /// congestion controlled" a request that is unlikely to succeed.
+    ///
+    /// # Default
+    ///
+    /// By default, this is `Require`. Changes to default values are **not** considered a breaking
+    /// change.
     CongestionControl,
 
     /// Interface instance or type
@@ -138,17 +182,22 @@ pub enum SelectionProperty {
     /// multiple paths between the same end hosts. Generally, in most cases, this will improve
     /// performance (e.g., achieve greater throughput).  One possible side-effect is increased
     /// jitter, which may be problematic for delay-sensitive applications.
-    Multipath,
-
-    /// Direction of communication
     ///
-    /// TODO
-    //Direction(Direction),
+    /// # Default
+    ///
+    /// By default, this is `Prefer`. Changes to default values are **not** considered a breaking
+    /// change.
+    Multipath,
 
     /// Notification of excessive retransmissions
     ///
     /// This property specifies whether an application considers it useful to be informed in case
     /// sent data was retransmitted more often than a certain threshold.
+    ///
+    /// # Default
+    ///
+    /// By default, this is `Ignore`. Changes to default values are **not** considered a breaking
+    /// change.
     RetransmitNotify,
 
     /// Notification of ICMP soft error message arrival
@@ -158,17 +207,24 @@ pub enum SelectionProperty {
     /// true, received ICMP errors will be available as SoftErrors. Note that even if a protocol
     /// supporting this property is selected, not all ICMP errors will necessarily be delivered, so
     /// applications cannot rely on receiving them.
+    ///
+    /// # Default
+    ///
+    /// By default, this is `Ignore`. Changes to default values are **not** considered a breaking
+    /// change.
     SoftErrorNotify,
 }
 
 pub struct TransportProperties {
-    map: EnumMap<SelectionProperty, Preference>
+    select_props: EnumMap<SelectionProperty, Preference>,
+    direction: Direction
 }
 
 impl Default for TransportProperties {
     fn default() -> Self {
         TransportProperties {
-            map: DEFAULT.clone()
+            select_props: DEFAULT.clone(),
+            direction: Default::default()
         }
     }
 }
@@ -185,11 +241,17 @@ impl fmt::Display for Preference {
     }
 }
 
+impl Default for Direction {
+    fn default() -> Self {
+        Direction::Bidirectional
+    }
+}
+
 impl TransportProperties {
     /// Specify the [Preference](enum.Preference.html) for the given
     /// [SelectionProperty](enum.SelectionProperty.html).
     pub fn add(&mut self, prop: SelectionProperty, pref: Preference) -> &mut Self {
-        self.map[prop] = pref;
+        self.select_props[prop] = pref;
         self
     }
 
@@ -234,5 +296,14 @@ impl TransportProperties {
     pub fn default(&mut self, prop: SelectionProperty) -> &mut Self {
         let pref = DEFAULT[prop];
         self.add(prop, pref)
+    }
+
+    /// Specify whether an application wants to use the Connection for sending and/or reding data.
+    ///
+    /// If `Sender` or `Receiver` is given, and the unidirectional connections are not supported by
+    /// the transport system, `Bidirectional` will be used as a fallback.
+    pub fn direction(&mut self, dir: Direction) -> &mut Self {
+        self.direction = dir;
+        self
     }
 }
